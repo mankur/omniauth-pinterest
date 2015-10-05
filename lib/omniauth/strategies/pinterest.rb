4f233cad20fa4fdeaf60e471b2751fbea9375d48
Ankur <amsystango@gmail.com>
2015-10-05 15:01:51 +0530

    Update auth urls and raw info

--- lib/omniauth/strategies/pinterest.rb
+++ lib/omniauth/strategies/pinterest.rb
@@ -4,9 +4,9 @@ module OmniAuth
   module Strategies
     class Pinterest < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :site => 'https://pinterest.com',
-        :authorize_url => 'https://pinterest.com/oauth/authorize',
-        :token_url => 'https://api.pinterest.com/v2/oauth/access_token'
+        :site => 'https://api.pinterest.com',
+        :authorize_url => '/oauth',
+        :token_url => '/v1/oauth/token'
       }
 
       def request_phase
@@ -15,19 +15,6 @@ module OmniAuth
         super
       end
 
-      uid { raw_info['id'] }
-
-      info do
-        {
-          'nickname' => raw_info['username'],
-          'name'     => raw_info['full_name'],
-          'image'    => raw_info['image_url'],
-        }
-      end
-
-      def raw_info
-        @data ||= access_token.params["user"]
-      end
     end
   end
 end
